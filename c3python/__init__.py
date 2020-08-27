41a5e958660c9850466f1a779076cb27928000ba
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-08-27 16:36:32 -0500

    Turn EvaluateResult types to Pandas DataFrames.
    Added the helper function EvaluateResultToPandas which changes
    an EvaluateResult into a Pandas DataFrame.

--- c3python/__init__.py
+++ c3python/__init__.py
@@ -1,4 +1,4 @@
-__all__ = [ 'get_c3' ]
+__all__ = [ 'get_c3' , 'EvaluateResultToPandas']
 __version__ = '0.1'
 
 def get_c3(url, tenant, tag, mode='thick', define_types=True, auth=None):
@@ -22,3 +22,58 @@ def get_c3(url, tenant, tag, mode='thick', define_types=True, auth=None):
         auth=auth,
         define_types=define_types
     )
+
+def EvaluateResultToPandas(result=None, eval_spec=None):
+    """
+    Take an EvaluateResult type and build a usable Pandas DataFrame from it.
+    If you pass the EvaluateSpec you used to eval_spec (either c3.EvaluateSpec
+    or dict type), this function will name the columns correctly as well.
+
+    Arguments:
+      result: A c3.EvaluateResult type which contains the results you want to
+        turn into a Pandas DataFrame. [Required]
+      eval_spec: Either a dict or c3.EvaluateSpec containing the spec you used
+        to get the results. If no eval_spec is passed, by default the columns
+        will be named 'c0', 'c1', ...
+
+    Returns:
+      A Pandas DataFrame containing the EvaluateResult data.
+    """
+
+    # Check whether the input is None
+    if result is None:
+        raise ValueError("You must pass a non-None value.")
+
+    # Check whether the input is the right type
+    if type(result) is not c3.EvaluateResult:
+        raise ValueError("You must pass a c3.EvaluateResult type.")
+
+    # Find number of columns in the result
+    num_columns = len(result.tuples[0].cells)
+    
+    # Get column names
+    if eval_spec is None:
+        column_names = ['c{}'.format(i) for i in range(num_columns)]
+    else:
+        if type(eval_spec) is dict:
+            column_names = eval_spec['projection'].split(',')
+        elif type(eval_spec) is c3.EvaluateResult:
+            column_names = eval_spec.projection.split(',')
+        else:
+            raise RuntimeError("eval_spec should be either a dict or c3.EvaluateType, Type {} not supported".format(type(eval_spec)))
+
+        if len(column_names) != num_columns:
+            raise RuntimeError("Eval Spec doesn't have correct number of columns! Has {} expected {}. Did you use the right Spec?".format(len(column_names), num_columns))
+
+    # Initialize the results dict
+    results = {}
+    for col in column_names:
+        results[col] = []
+    
+    # Fill the results dict
+    for row in result.tuples:
+        for i in range(num_columns):
+            results[column_names[i]].append(row.cells[i].value())
+    
+    # Build and return the final Pandas DataFrame
+    return pd.DataFrame(results) 
