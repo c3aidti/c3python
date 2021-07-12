93509594391cd666012cc4d77700ca27150138f6
Darren Adams <dadams@illinois.edu>
2021-07-12 14:16:55 -0500

    working test version of signer

--- c3python/c3python.py
+++ c3python/c3python.py
@@ -1,10 +1,53 @@
+import os
+from time import time
+import json
 
+import base64
+from Crypto.Hash import SHA512
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+def _get_key(PEM_LOCATION):
+    with open(PEM_LOCATION, 'rb') as secret_file:
+        secret=secret_file.read()
+        if (secret.startswith(b'-----BEGIN RSA PRIVATE KEY-----') or
+            secret.startswith(b'-----BEGIN PRIVATE KEY-----')):
+            # string with PEM encoded key data
+            k = secret
+            rsa_key = RSA.importKey(k)
+            return PKCS1_v1_5.new(rsa_key)
+        else:
+            return None
+def _get_sig(signer, sigtext):
+    """
+    Return the signature.
+    """
+    pass
 
-def _get_c3_key_token(keyfile=None, keystring=None, sigtext=None):
+def _get_c3_key_token(keyfile=None, keystring=None, signature_text=None, username=None):
     """
     Return the key token for the c3 keyfile.
     """
-    pass
+    if not keyfile and not keystring:
+        keyfile = os.getenv('HOME') + '/.c3/c3-rsa'
+
+    if not signature_text:
+      signature_text = str(int(time()*1000))
+    
+    key = _get_key(keyfile)
+    print (signature_text)
+    #data=json.dumps(signature_text)
+    h=SHA512.new()
+    h.update(signature_text.encode('utf-8'))
+    sig = key.sign(h)
+    plainsig = base64.b64encode(sig).decode('utf-8')
+    print(f"pysig:{plainsig}")
+    #tokenString = adminUser + ":" + Buffer.from(signatureText).toString('base64') + ":" + signature;
+    tokenstr = f"{username}:{base64.b64encode(signature_text.encode('utf-8')).decode('utf-8')}:{plainsig}"
+    print(f"pytokenstr:{tokenstr}")
+    authtoken = f"c3key {base64.b64encode(tokenstr.encode('utf-8')).decode('utf-8')}"
+    print(f"pyauthtoken:{authtoken}")
+    token = sig
+    return token
 
 def getc3():
     """
--- test/c3python/nodetoken/bin/index.js
+++ test/c3python/nodetoken/bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // This file is for testing purposed only to compare signing
 // algorithms between node.js and python.
-console.log( "Hello!" );
+//console.log( "Hello!" );
 // lib here is lib from c3.js
 var lib = {
 crypto : require('crypto'),
@@ -31,31 +31,33 @@ var getFileContents = function(file) {
   };
 
 var keyFile = process.env.HOME + "/.c3/c3-rsa"
-console.log(keyFile)
+//console.log(keyFile)
 var pvtKey = getFileContentsIfPossible(keyFile);
-console.log(pvtKey)
+//console.log(pvtKey)
 
 //var pvtKey = '-----BEGIN RSA PRIVATE KEY-----\
 //some random key with a bunch of stuff in it.\
 //-----END RSA PRIVATE KEY-----\
 //';
 var crypto = lib.crypto;
-var adminUser = " a user";
+var adminUser = "auser";
   
 function getC3KeyToken() {
       if (!pvtKey) throw new Error("Cannot generate c3key auth token with private key");
   
       var signAlgo = 'RSA-SHA512';
-      var signatureText = Date.now().toString();
+      //var signatureText = Date.now().toString();
+      var signatureText = '1626099164960'
+      //console.log (signatureText);
       var signer = crypto.createSign(signAlgo);
       signer.update(signatureText);
       var signature = signer.sign(pvtKey, 'base64');
+      console.log('signature::'+signature);
       var tokenString = adminUser + ":" + Buffer.from(signatureText).toString('base64') + ":" + signature;
+      console.log('tokenString::'+tokenString);
       var authToken = "c3key " + Buffer.from(tokenString).toString('base64');
-  
       //log.verbose("Generated new token: " + authToken);
-      console.log(authToken);
-      
+      console.log('authToken::'+authToken);
       return authToken;
     };
 
